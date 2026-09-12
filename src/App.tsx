be6803c07284d5f9921904c6bd83c4fbbb44b7db
Aleksandr Mazhul <map07102007@gmail.com>
2026-09-12 20:39:52 +0300

    checkpoint before checking out main

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
-import type { Note } from './types';
+import type {Note} from './types';
 import {useState} from "react";
 
 function App() {
-  const [note ] = useState<Note[]>([{
-    title: 'My First Note',
-    content: 'This is the content of my first note.',
-    createdAt: new Date(),
-    hidden: false
-  }]);
+  const [notes] = useState<Note[]>(() => {
+    return [
+      {
+        title: 'My first note',
+        content: 'This is the content of my first note.',
+        createdAt: new Date(),
+        hidden: false,
+      },
+      {
+        title: 'My second note',
+        content: 'This is the content of my second note.',
+        createdAt: new Date(),
+        hidden: false,
+      },
+      {
+        title: 'My third note',
+        content: 'This is the content of my third note.',
+        createdAt: new Date(),
+        hidden: false,
+      }
+    ] satisfies Note[];
+  });
 
   return (
-    <div className="App">
-
-      <header className="App-header">
-        <h1>Welcome to My App</h1>
-      </header>
+    <div>
+      <h1>My Notes</h1>
+      {notes.map((note, index) => (
+        <div key={index}>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
+          <p>{note.createdAt.toLocaleString()}</p>
+          <p>{note.hidden ? 'Hidden' : 'Visible'}</p>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App
